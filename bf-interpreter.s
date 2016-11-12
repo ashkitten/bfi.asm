a87b3ba8367478a3b1413fc581302191aa1e5822
Ash Levy <ashy.of.ren@gmail.com>
2016-11-11 20:30:07 -0500

    iterate character by character, exit if character is '!' (TEMPORARY)

--- bf-interpreter.s
+++ bf-interpreter.s
@@ -3,9 +3,7 @@ section .data
 	msg1_size equ $-msg1		; first prompt length
 
 section .bss
-	buffer resb 256
-	buffer_size equ $-buffer	; buffer size
-	count resb 1
+	buffer resb 1
 
 section .text
 	global _start
@@ -25,25 +23,27 @@ _start:
 readwrite:
 	.1:
 		; sys_read
-		mov	edx, buffer_size
+		mov	edx, 1
 		mov	ecx, buffer
 		mov	ebx, 0			; stdin
 		mov	eax, 3			; sys_read
 		int	80h			; syscall
 
-		mov	[count], eax		; save count for later
-
 		; print what we got from sys_read
 		; sys_write
-		mov	edx, [count]
+		mov	edx, 1
 		mov	ecx, buffer
 		mov	ebx, 1			; stdout
 		mov	eax, 4			; sys_write
 		int	80h			; syscall
 
+		cmp	byte[ecx], 33		; if buffer character is a "!"
+		je	exit
+
+		; this is so if we overflow the buffer then it'll read more
 		; if the last byte we printed is a linefeed, we're done
 		; if not, there must be more to read
-		cmp	byte [ecx + edx - 1], 10
+		cmp	byte[ecx], 10
 		jne	.1
 
 		ret
